b23f880bc0b1cd4f493839a260b6194cedbd79b2
Javier Negre <javi.negre@gmail.com>
2020-11-22 23:36:10 +0100

    Allow editing favorites in dialog

--- src/app/favorite-item-list/favorite-item-list.component.ts
+++ src/app/favorite-item-list/favorite-item-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FavoriteItemListService } from '../favorite-item-list.service';
 import { IFavoriteItem, IItem, IListFilterConfig, IListRefinementConfig } from '../../models/interfaces';
 import { ItemListService } from '../item-list.service';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-favorite-item-list',
@@ -11,12 +12,15 @@ import { ItemListService } from '../item-list.service';
 export class FavoriteItemListComponent implements OnInit {
   itemList: IItem[] | undefined;
   favoriteItemList: IFavoriteItem[] | undefined;
+  removedItems: IFavoriteItem[] | undefined;
 
   searchCriteria: IListFilterConfig | undefined;
 
   constructor(private itemListService: ItemListService, private favoriteItemListService: FavoriteItemListService) {}
 
   ngOnInit(): void {
+    this.removedItems = [];
+
     this.getFavoriteItemList();
   }
 
@@ -32,10 +36,36 @@ export class FavoriteItemListComponent implements OnInit {
   }
 
   getFavoriteItemList(): void {
-    this.favoriteItemListService.getFavoriteItemList().subscribe((favoriteItemList) => {
-      this.favoriteItemList = favoriteItemList;
-      this.getItemList();
-    });
+    this.favoriteItemListService
+      .getFavoriteItemList()
+      .pipe(first())
+      .subscribe((favoriteItemList) => {
+        this.favoriteItemList = favoriteItemList;
+        this.getItemList();
+      });
+  }
+
+  addToFavoriteList(item: IItem): void {
+    this.favoriteItemListService.addItem(item.uid).subscribe();
+
+    const itemToAdd: IFavoriteItem | undefined = this.removedItems?.find((it) => it.uid === item.uid);
+    if (itemToAdd) {
+      this.favoriteItemList?.push(itemToAdd);
+    }
+  }
+
+  deleteFromFavoriteList(item: IItem): void {
+    const favoriteItem = this.favoriteItemList?.find((it) => it.uid === item.uid);
+    if (favoriteItem) {
+      this.favoriteItemListService.deleteItem(favoriteItem).subscribe();
+
+      this.favoriteItemList = this.favoriteItemList?.filter((it) => it.uid !== item.uid);
+      this.removedItems?.push(favoriteItem);
+    }
+  }
+
+  isFavoriteItem(item: IItem): boolean {
+    return this.favoriteItemList?.find((it) => it.uid === item.uid) !== undefined;
   }
 
   searchCriteriaChanged(searchCriteria: IListFilterConfig): void {
